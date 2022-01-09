68b6d384492019ba2588dcb4b839c4b4fad55065
Claudi0-V <claudiov.dejesus@protonmail.com>
2022-01-09 12:44:05 -0300

    adds sign-up routing and user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    min: 5,
+    max: 25,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  }
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,11 +1,41 @@
 const router = require("express").Router();
-
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.js");
+console.log(jwt);
 
 router.get("/login", (req, res) => {
-  console.log("request recieved");
-  res.status(200).json({it: "works"})
+  res.status(200).json({ it: "works" });
 });
 
+const createToken = (_id) => {
+  const maxAge = 3 * 24 * 60 * 60;
+  return jwt.sign({ _id }, process.env.SECRET_KEY, {
+    expiresIn: maxAge,
+  });
+};
 
+router.post("/sign-up", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const hasUser = await User.findOne({ email });
+    if (!!hasUser) {
+      return res.status(401).json({ err: " User Already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({
+      username,
+      email,
+      hashedPassword,
+    });
+    await user.save();
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({user: user._id});
+  } catch (err) {
+    res.status(401).json({ err: err });
+  }
+});
 
-module.exports = router
+module.exports = router;

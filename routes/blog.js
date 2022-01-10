8e38cc5c04928d159783800ff31dd3247c8f481e
Claudi0-V <claudiov.dejesus@protonmail.com>
2022-01-10 20:12:44 -0300

    adds put and delete routes, and change the  post route to accept new auth

--- routes/blog.js
+++ routes/blog.js
@@ -1,24 +1,41 @@
 const router = require("express").Router();
 const Blog = require("../models/blogs.js");
 const User = require("../models/user.js");
-const auth = require("../middleware/auth.js");
+const { jwt_auth, isAdmin } = require("../middleware/auth.js");
 
-router.get("/blogs", async (req, res) => {
+router.get("/", async (req, res) => {
   const blogs = await Blog.find();
   if (blogs.length > 0) res.status(200).json(blogs);
   else res.status(200).json({ err: "there's no blogs currently" });
 });
 
+router.post("/", jwt_auth, isAdmin, async (req, res) => {
+  const { user, isAdmin } = req;
+  const { title, body } = req.body;
+  const blog = new Blog({ title, body });
+  await blog.save();
+  res.status(200).json({ msg: "Blog Saved" });
+});
 
-router.post("/blogs", auth.jwt_auth, async (req, res) => {
-  const {user, isAdmin} = req
-  if (isAdmin) {
-    const { title, body } = req.body;
-    const blog = new Blog({ title, body });
-    await blog.save();
-    res.status(200).json({ msg: "Blog Saved", ...blog });
-  } else {
-    res.status(401).json({ err: "User not authorized" });
+router.delete("/", jwt_auth, isAdmin, async (req, res) => {
+  const { user } = req;
+  const _id = req.body;
+  try {
+    const blog = await Blog.findByIdAndDelete(_id);
+    res.status(200).json({ sucess: "Blog post deleted" });
+  } catch (err) {
+    res.status(401).json({ err: "Blog with this ID was not found" });
+  }
+});
+
+router.put("/", jwt_auth, isAdmin, async (req, res) => {
+  const { user } = req;
+  const { _id, post } = req.body;
+  try {
+    const blog = await Blog.findByIdAndUpdate({ _id }, post );
+    res.status(201).json({ sucess: "Blog succefuly updated", blog });
+  } catch (err) {
+    res.status(401).json({ err: "Blog with this ID was not found" });
   }
 });
 
